55d4ada0883764208539a1b9faaca7be4bc8283b
Siddhi Manjarekar <114218813+Siddhi-m-20@users.noreply.github.com>
2023-09-11 08:14:36 +0530

    Add files via upload

---
+++ Assignment1/array_malloc.c
@@ -0,0 +1,72 @@
+/*Accept n integers in an array. Copy only the non-zero elements to another array (allocated
+using dynamic memory allocation). Calculate the sum and average of non-zero elements*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, i, j, sum = 0,count = 0;;
+    int *arr, *copy;
+    double avg;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    arr = (int *)malloc(n * sizeof(int));
+
+    printf("Enter %d elements in array: \n", n);
+    for (i = 0; i < n; i++)
+    {
+        //scanf("%d", arr+i);
+        *(arr+i)=rand() % 100; 
+    }
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", *(arr+i));
+    }
+    printf("\n");
+
+
+    
+    for (i = 0; i < n; i++)
+    {
+        if (*(arr+i)!= 0)
+        {
+            count++;
+        }
+    }
+
+    copy = (int *)malloc(count * sizeof(int));
+
+    j = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (*(arr+i) != 0)
+        {
+            *(copy+j) = *(arr+i);
+            j++;
+        }
+    }
+
+    printf("The copy array is: ");
+    for (i = 0; i < count; i++)
+    {
+        printf("%d ", *(copy+i));
+    }
+    printf("\n");
+
+    for (i = 0; i < count; i++)
+    {
+        sum += *(copy+i);
+    }
+    
+    avg = (double)sum / count;
+
+    printf("The sum of non-zero elements is: %d\n", sum);
+    printf("The average of non-zero elements is: %.2f\n", avg);
+
+    free(arr);
+    free(copy);
+
+    return 0;
+}
