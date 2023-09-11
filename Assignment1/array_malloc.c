402d7fa5ea9a11527fef4220bf29fda89901009f
Siddhi Manjarekar <114218813+Siddhi-m-20@users.noreply.github.com>
2023-09-11 08:46:50 +0530

    Update array_malloc.c

--- Assignment1/array_malloc.c
+++ Assignment1/array_malloc.c
@@ -6,44 +6,55 @@ using dynamic memory allocation). Calculate the sum and average of non-zero elem
 
 int main()
 {
-    int n, i, j, sum = 0,count = 0;;
+    int n, i, j, sum = 0, count = 0;
+    ;
     int *arr, *copy;
     double avg;
     printf("Enter size of array: ");
     scanf("%d", &n);
 
     arr = (int *)malloc(n * sizeof(int));
+    if (arr == NULL)
+    {
+
+        printf("Memory allocation failed for arr\n");
+        return -1;
+    }
 
     printf("Enter %d elements in array: \n", n);
     for (i = 0; i < n; i++)
     {
-        //scanf("%d", arr+i);
-        *(arr+i)=rand() % 100; 
+        // scanf("%d", arr+i);
+        *(arr + i) = rand() % 100;
     }
     for (i = 0; i < n; i++)
     {
-        printf("%d ", *(arr+i));
+        printf("%d ", *(arr + i));
     }
     printf("\n");
 
-
-    
     for (i = 0; i < n; i++)
     {
-        if (*(arr+i)!= 0)
+        if (*(arr + i) != 0)
         {
             count++;
         }
     }
 
     copy = (int *)malloc(count * sizeof(int));
+    if (copy == NULL)
+    {
+        printf("Memory allocation failed for copy\n");
+        free(arr);
+        return -1;
+    }
 
     j = 0;
     for (i = 0; i < n; i++)
     {
-        if (*(arr+i) != 0)
+        if (*(arr + i) != 0)
         {
-            *(copy+j) = *(arr+i);
+            *(copy + j) = *(arr + i);
             j++;
         }
     }
@@ -51,15 +62,15 @@ int main()
     printf("The copy array is: ");
     for (i = 0; i < count; i++)
     {
-        printf("%d ", *(copy+i));
+        printf("%d ", *(copy + i));
     }
     printf("\n");
 
     for (i = 0; i < count; i++)
     {
-        sum += *(copy+i);
+        sum += *(copy + i);
     }
-    
+
     avg = (double)sum / count;
 
     printf("The sum of non-zero elements is: %d\n", sum);
